3677e73f16978462d958d696b02bef76e8f8d2e1
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-25 22:34:20 +0300

    Adding MatrixOperation.hpp

---
+++ MatrixOperation.hpp
@@ -0,0 +1,21 @@
+#include "Operation.hpp"
+#include "Matrix.hpp"
+
+namespace operation {
+
+    class MatrixOperation : public Operation {
+        
+        // the left operand matrix
+        Matrix _leftOperand;
+        // the right operand matrix
+        Matrix _rightOperand;
+        // the output matrix
+        Matrix _output;
+
+        MatrixOperation(const Matrix& leftOperand, const Matrix& rightOperand, const Matrix& output, std::string info);
+
+        virtual void writeOperationToOutputFile(const std::string& outputPath) const override;
+
+        virtual void addOperationFileToCache() const override;
+    }
+}
