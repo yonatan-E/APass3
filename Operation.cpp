863781e23d54b63f203916c24c8f9a9929b5c9d5
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-26 01:08:40 +0300

    Updating ctor at Operation.cpp

--- Operation.cpp
+++ Operation.cpp
@@ -6,7 +6,8 @@
 namespace operation {
 
     Operation::Operation(std::string info) 
-        : _info(std::move(info)), _cachePath(std::move(std::string(_info).replace(_info.find(" "), sizeof(_info), "_"))) {}
+        : _info(std::move(info)), 
+        _cachePath(std::move("cache/" + std::string(_info).replace(_info.find(" "), sizeof(_info), "_"))) {}
 
     void Operation::deleteOperationFileFromCache() const {
         if (remove(_cachePath.c_str()) != 0) {
--- OperationExceptions.cpp
+++ OperationExceptions.cpp
@@ -3,6 +3,7 @@
 
 namespace operation {
     namespace exceptions {
+        
         FileDeleteException::FileDeleteException() 
             : std::runtime_error("Error while deleting file") {}
 
