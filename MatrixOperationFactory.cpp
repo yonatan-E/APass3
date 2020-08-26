ce487cb178241288230a72769d7496228e45f47a
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-26 22:17:23 +0300

    Adding MatrixOperationFactory.cpp

---
+++ MatrixOperationFactory.cpp
@@ -0,0 +1,13 @@
+#include "MatrixOperationFactory.hpp"
+#include <string>
+
+namespace operation {
+
+    const Operation& MatrixOperationFactory::createOperation(const std::string command[]) const {
+
+    }
+
+    bool MatrixOperationFactory::isValidCommand(const std::string command[]) const {
+
+    }
+}
