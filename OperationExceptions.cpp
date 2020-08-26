72f8f40848d3c018a3b1a7aefe408e1c2706e1e0
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-26 19:36:21 +0300

    Adding FileWriteException and FileReadException

--- OperationExceptions.cpp
+++ OperationExceptions.cpp
@@ -9,5 +9,11 @@ namespace operation {
 
         FileOpenException::FileOpenException() 
             : std::runtime_error("Error while opening file") {}
+
+        FileWriteException::FileWriteException()
+            : std::runtime_error("Error while writing to file") {}
+
+        FileReadException::FileReadException()
+            : std::runtime_error("Error while reading from file") {}
     }
 }
