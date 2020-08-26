b6c81a9ce0c6ddf42e30ee01696314d44449c6b7
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-26 19:31:06 +0300

    Adding implementation to addOperationToCache()

--- BitmapOperation.cpp
+++ BitmapOperation.cpp
@@ -1,6 +1,7 @@
 #include "BitmapOperation.hpp"
 #include "OperationExceptions.hpp"
 #include <stdexcept>
+#include <fstream>
 
 namespace operation {
 
@@ -18,6 +19,32 @@ namespace operation {
     }
 
     void BitmapOperation::addOperationFileToCache() const {
+        // opening the cache file using ofstream
+		std::ofstream cacheFile(getCachePath(), std::ios::binary | std::ios::trunc);
         
+        // checking if an error has occured while opening the file
+		if (!cacheFile.is_open()) {
+			throw exceptions::FileOpenException();
+		}
+
+		// writing the data string of the input bitmap file into the cache file
+		cacheFile.write(_input.getData().data(), static_cast<std::streamsize>(_input.getData().length()));
+        // checking if an error has occured while writing to the file
+		if (!cacheFile) {
+			throw exceptions::FileWriteException();
+		}
+
+        // line seperator
+        cacheFile << '\n';
+
+        // writing the data string of the output bitmap file into the cache file
+		cacheFile.write(_output.getData().data(), static_cast<std::streamsize>(_output.getData().length()));
+        // checking if an error has occured while writing to the file
+		if (!cacheFile) {
+			throw exceptions::FileWriteException();
+		}
+
+        // closing the ofstream
+        cacheFile.close();
     }
 }
