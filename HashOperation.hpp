3280a8fd2d90a1ec3ea33555a635380edba26c10
Ilan Prais <ilandovprais@gmail.com>
2020-08-27 00:07:52 +0300

    Adding _input member

--- HashOperation.hpp
+++ HashOperation.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
 #include "Operation.hpp"
+#include "CrcHash.hpp"
 
 namespace operation {
 
     class HashOperation : public Operation {
 
-            HashOperation(std::string info);
+            HashOperation(std::string info, const std::string& input);
 
             virtual void writeOperationToOutputFile(const std::string& outputPath) const override;
 
             virtual void addOperationFileToCache() const override;
 
         private:
+
+            hash::CrcHash _input;
 
     };
 }
