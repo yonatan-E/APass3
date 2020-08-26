12886b4a92fdc262009333a337291c89d5e04c13
Ilan Prais <ilandovprais@gmail.com>
2020-08-27 00:40:25 +0300

    Adding getInput method

--- CrcHash.cpp
+++ CrcHash.cpp
@@ -6,6 +6,10 @@ namespace hash{
     CrcHash::CrcHash(const std::string input) : _input(input){}
 
     std::string CrcHash::applyAlgorithm(){
-        
+
+    }
+
+    std::string CrcHash::getInput(){
+        return _input;
     }
 }
