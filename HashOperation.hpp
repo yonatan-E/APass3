7b46ea58d2f506ae777315e45dc51581e7be44a8
Ilan Prais <ilandovprais@gmail.com>
2020-08-27 00:29:14 +0300

    Changing ctor to recieve CrcHash instead of string

--- HashOperation.hpp
+++ HashOperation.hpp
@@ -7,7 +7,7 @@ namespace operation {
 
     class HashOperation : public Operation {
 
-            HashOperation(std::string info, const std::string& input);
+            HashOperation(std::string info, const hash::CrcHash& input);
 
             virtual void writeOperationToOutputFile(const std::string& outputPath) const override;
 
