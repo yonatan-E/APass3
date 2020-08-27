29651a8925dc74f7614212bfd3386405d2ade152
Ilan Prais <ilandovprais@gmail.com>
2020-08-27 13:03:26 +0300

    Implementing the applyAlgorithm method

--- CrcHash.cpp
+++ CrcHash.cpp
@@ -1,12 +1,13 @@
 #include "CrcHash.hpp"
-#include"string"
+#include "string"
+#include "crc32.h"
 
 namespace hash{
 
     CrcHash::CrcHash(const std::string input) : _input(input){}
 
     std::string CrcHash::applyAlgorithm(){
-
+        return calculate_crc32c(0, (char*) _input, _input.size());
     }
 
     std::string CrcHash::getInput(){
