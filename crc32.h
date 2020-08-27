1d735f2f1e44154aa924fdc625fe9639276dcd3f
Ilan Prais <ilandovprais@gmail.com>
2020-08-27 13:02:58 +0300

    Adding the crc algorithm files

---
+++ crc32.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+
+
+#include <sys/param.h>
+#include <sys/systm.h>
+
+uint32_t
+calculate_crc32c(uint32_t crc32c,
+    const unsigned char *buffer,
+    unsigned int length);
+
+
+#ifdef __cplusplus
+}
+#endif
