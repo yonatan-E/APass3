bc4c198f118a9ef9f19446fad44739fbf786f40b
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-27 22:45:20 +0300

    Changing ctor and adding getInfo()

--- Operation.hpp
+++ Operation.hpp
@@ -4,14 +4,12 @@ namespace operation {
 
     class Operation {
 
-        // the path to the file in the cache
-        std::string _cachePath;
         // the time and the date when the operation happened
         std::string _info;
+        // the path to the file in the cache
+        std::string _cachePath;
 
         public:
-
-            Operation(std::string info);
             
             virtual void writeOperationToOutputFile(const std::string& outputPath) const = 0;
 
@@ -19,6 +17,9 @@ namespace operation {
 
             virtual void deleteOperationFileFromCache() const final;
 
+            virtual const std::string& getInfo() const final;
+
             virtual const std::string& getCachePath() const final; 
+
     };
 }
