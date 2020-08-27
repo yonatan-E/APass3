567c3aa52ba9a02e398e2ff1e8cdf82c74c909f3
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-27 17:50:40 +0300

    Added implementation to isValidComand() *not final*

--- MatrixOperationFactory.cpp
+++ MatrixOperationFactory.cpp
@@ -12,7 +12,13 @@ namespace operation {
     }
 
     bool MatrixOperationFactory::isValidCommand(const std::string command[]) const {
-
+        // checking if the command is valid, and throwing an exception if it isn't
+        if (command[0] != "matrix" || (command[1] != "add" && command[1] != "multiply")
+        || command[2].substr(command[2].size() - 4, 4) != ".txt"
+        || command[3].substr(command[3].size() - 4, 4) != ".txt"
+        || (command[4].substr(command[2].size() - 4, 4) != ".txt" && command[4] != "stdout")) {
+            throw exceptions::InvalidCommandException();
+        }
     }
 
     const matrix::Matrix MatrixOperationFactory::readMatrixFromFile(const std::string& pathToFile) {
