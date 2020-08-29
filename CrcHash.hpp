3421f3d275e132559f8501949935a1f1af78525e
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-29 15:41:25 +0300

    Removing unnnesecerry operator== from CrcHash

--- CrcHash.hpp
+++ CrcHash.hpp
@@ -12,8 +12,6 @@ namespace hash {
 
             CrcHash(std::string input);
 
-            bool operator==(const CrcHash& other) const;
-
             std::string applyAlgorithm() const;
 
             const std::string& getInput() const;
