ae9e6484e5f2d7821eb6432f31089473fec88971
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-29 18:46:58 +0300

    Little change in applyAlgorithm()

--- CrcHash.cpp
+++ CrcHash.cpp
@@ -7,7 +7,7 @@ namespace hash {
     CrcHash::CrcHash(std::string input) : _input(std::move(input)) {}
 
     uint32_t CrcHash::applyAlgorithm() const {
-        return calculate_crc32c(0, (char*)_input, _input.size());
+        return calculate_crc32c(0, (const unsigned char*)_input.c_str(), _input.size());
     }
 
     const std::string& CrcHash::getInput() const {
