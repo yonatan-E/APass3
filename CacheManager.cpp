94d0613545122f9abea8b6f491b79b4cbac826ca
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-30 02:21:09 +0300

    Fixing all methods

--- CacheManager.cpp
+++ CacheManager.cpp
@@ -2,49 +2,47 @@
 #include "OperationExceptions.hpp"
 #include <fstream>
 #include <algorithm>
+#include <iterator>
 
 namespace cache {
 
     CacheManager::CacheManager(uint32_t maxSize, std::string pathToInfoFile)
-    : _hashCodes(maxSize), _pathToInfoFile(std::move(pathToInfoFile)) {
+    : _maxSize(maxSize), _pathToInfoFile(std::move(pathToInfoFile)) {
         
         std::ifstream info(_pathToInfoFile);
 
         if (!info.is_open()) {
-            throw operation::exceptions::FileOpenException();
+            std::ofstream temp(_pathToInfoFile);
+            temp.close();
         }
 
-        char* line;
-        while (info.getline(line, sizeof(uint32_t), '\n')) {
-            try {
-                _hashCodes.push_back(std::stoi(line));
-            } catch (...) {
-                throw operation::exceptions::FileFormatException();
-            } 
-        }
+        std::copy(std::istream_iterator<uint32_t>(info), std::istream_iterator<uint32_t>(), std::back_inserter(_hashCodes));
 
         info.close();
     }
 
-    bool CacheManager::contains(const operation::Operation& operation) {
-        return std::find(_hashCodes.begin(), _hashCodes.end(), operation.getHashCode()) != _hashCodes.end();
+    bool CacheManager::contains(uint32_t hashCode) const {
+        return std::find(_hashCodes.begin(), _hashCodes.end(), hashCode) != _hashCodes.end();
     }
 
     void CacheManager::add(const operation::Operation& operation) {
-        if (!contains(operation)) {
-            _hashCodes.push_back(operation.getHashCode());
-            
-            if (_hashCodes.size() >= _hashCodes.max_size()) {
+
+        if (!contains(operation.getHashCode())) {
+
+            if (_hashCodes.size() >= _maxSize) {
+
+                if (remove(("cache/" + std::to_string(*(_hashCodes.begin())) + ".txt").c_str()) != 0) {
+                    throw operation::exceptions::FileDeleteException();
+                }
+
                 _hashCodes.erase(_hashCodes.begin());
             }
 
             _hashCodes.push_back(operation.getHashCode());
 
         } else {
-            std::vector<uint32_t>::iterator current = 
-                std::find(_hashCodes.begin(), _hashCodes.end(), operation.getHashCode());
-            _hashCodes.erase(current);
-            _hashCodes.push_back(*current);
+            _hashCodes.erase(std::find(_hashCodes.begin(), _hashCodes.end(), operation.getHashCode()));
+            _hashCodes.push_back(operation.getHashCode());
         }
 
         std::ofstream info(_pathToInfoFile, std::ios::trunc);
@@ -53,8 +51,10 @@ namespace cache {
             throw operation::exceptions::FileOpenException();
         }
 
-        std::for_each(_hashCodes.begin(), _hashCodes.end(), [&info](std::vector<uint32_t>::iterator it) {
-            info << *it << '\n';
+        std::for_each(_hashCodes.begin(), _hashCodes.end(), [&info](uint32_t hash) {
+            info << hash << '\n';
         });
+
+        operation.addOperationFileToCache();
     }
 }
--- CacheManager.hpp
+++ CacheManager.hpp
@@ -8,12 +8,13 @@ namespace cache {
     class CacheManager {
 
         std::vector<uint32_t> _hashCodes;
+        uint32_t _maxSize;
         std::string _pathToInfoFile;
 
         public:
 
             CacheManager(uint32_t maxSize, std::string pathToInfoFile);
-            bool contains(const operation::Operation& operation);
+            bool contains(uint32_t hashCode) const;
             void add(const operation::Operation& operation);
     };
 }
