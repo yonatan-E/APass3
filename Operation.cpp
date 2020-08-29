e39c77bf18aacb0d4ea07f8e97a36db22fcbdb88
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-29 13:53:06 +0300

    Updating Operation class

--- Operation.cpp
+++ Operation.cpp
@@ -5,26 +5,24 @@
 
 namespace operation {
 
-    Operation::Operation() {
+    Operation::Operation(uint32_t hashCode) : _hashCode(hashCode) {
             // current date/time based on current system
             time_t now = time(0);
             // initializing the info string with current date/time
             _info = std::move(ctime(&now)); 
-            // initializing the path to the file in the cache
-            _cachePath = std::move("cache/" + std::string(_info).replace(_info.find(" "), sizeof(_info), "_"));
         }
 
     void Operation::deleteOperationFileFromCache() const {
-        if (remove(_cachePath.c_str()) != 0) {
+        if (remove("cache/" + _hashCode) != 0) {
             throw exceptions::FileDeleteException();
         } 
     }
 
-    const std::string& Operation::getInfo() const {
-        return _info;
+    uint32_t Operation::getHashCode() const {
+        return _hashCode;
     }
 
-    const std::string& Operation::getCachePath() const {
-        return _cachePath;
+    const std::string& Operation::getInfo() const {
+        return _info;
     }
 }
--- Operation.hpp
+++ Operation.hpp
@@ -1,28 +1,27 @@
 #include <string>
+#include <cstdint>
 
 namespace operation {
 
     class Operation {
 
+        // the hash code of the operation
+        uint32_t _hashCode;
         // the time and the date when the operation happened
         std::string _info;
-        // the path to the file in the cache
-        std::string _cachePath;
 
         public:
 
-            Operation();
+            Operation(uint32_t hashCode);
 
             virtual void writeOperationToOutputFile(const std::string& outputPath) const = 0;
 
             virtual void addOperationFileToCache() const = 0;
 
-            virtual bool operator==(const Operation& other) const = 0;
-
             virtual void deleteOperationFileFromCache() const final;
 
-            virtual const std::string& getInfo() const final;
+            virtual uint32_t getHashCode() const final;
 
-            virtual const std::string& getCachePath() const final; 
+            virtual const std::string& getInfo() const final; 
     };
 }
