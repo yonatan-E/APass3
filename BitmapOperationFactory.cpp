7503ebbae46a333d0cf2ddac0e5df0bef12457ac
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-29 15:36:41 +0300

    Updating BitmapOperationFactory according to OperationFactory

--- BitmapOperationFactory.cpp
+++ BitmapOperationFactory.cpp
@@ -5,27 +5,51 @@
 
 namespace operation {
 
-    std::unique_ptr<Operation> BitmapOperationFactory::createOperation(const std::string command[]) const {
+    std::unique_ptr<Operation> BitmapOperationFactory::createOperation(const std::vector<std::string>& command,
+        cache::CacheManager& cache) const {
         // checking if the command is valid
         if (!isValidCommand(command)) {
             throw exceptions::InvalidCommandException();
         }
 
-        // getting the operation type
-        BitmapOperation::OperationType type = command[1] == "rotate" ? 
-            BitmapOperation::OperationType::rotate : BitmapOperation::OperationType::convert;
-        // getting the input bitmap file
+        // putting this code in a try block to catch the exception which can be thrown 
+        // while creating the bitmap object, and convert it to another exception
         try {
+            // getting the input bitmap
             bitmap::Bitmap input(command[2]);
-            return std::make_unique<Operation>(input, type);
+
+            // getting the hash code of the operation
+            uint32_t hashCode = getOperationHashCode(input, command[1]);
+
+            // if the operation is already exist on the cache, we will take the result of the operation
+            // from the cache, so we don't have to calculate it again
+            if (cache.contains(hashCode)) {
+                // getting the result bitmap from the cache
+                bitmap::Bitmap result("cache/" + hashCode);
+                // returning a unique pointer to the matrix operation
+                return std::make_unique<BitmapOperation>(hashCode, result);
+            }
+
+            // if the operation isn't on the cache, we will add it to the cache.
+            // first, calculating the result bitmap
+            bitmap::Bitmap result(input);
+            command[1] == "rotate" ? result.turn() : result.gray();
+            // getting the operation object
+            BitmapOperation operation(hashCode, result);
+            // adding the operation object to the cache
+            cache.add(operation);
+            // returning a smart pointer to the operation
+            return std::make_unique<BitmapOperation>(operation);
         } catch (...) {
+            // "converting" the exception which was thrown at the ctor of Bitmap while reading from the output file
+            // to out FileReadException
             throw exceptions::FileReadException();
         }
     }
 
-    bool BitmapOperationFactory::isValidCommand(const std::string command[]) const {
+    bool BitmapOperationFactory::isValidCommand(const std::vector<std::string>& command) const {
         // checking if the command is valid
-        if (sizeof(command) / sizeof(std::string) != 4 
+        if (command.size() != 4 
         || command[0] != "image" 
         || (command[1] != "rotate" && command[1] != "convert")
         || command[2].substr(command[2].size() - 4, 4) != ".bmp"
@@ -33,5 +57,9 @@ namespace operation {
             return false;
         }
         return true;
+    }
+
+    uint32_t getOperationHashCode(const bitmap::Bitmap& arg, const std::string& operationType) {
+
     }
 }
--- BitmapOperationFactory.hpp
+++ BitmapOperationFactory.hpp
@@ -9,10 +9,15 @@ namespace operation {
 
         public:
 
-            virtual std::unique_ptr<Operation> createOperation(const std::string command[]) const override;
+            virtual std::unique_ptr<Operation> createOperation(const std::vector<std::string>& command,
+                cache::CacheManager& cache) const override;
 
         protected:
 
-            virtual bool isValidCommand(const std::string command[]) const override;
+            virtual bool isValidCommand(const std::vector<std::string>& command) const override;
+
+        private:
+
+            static uint32_t getOperationHashCode(const bitmap::Bitmap& arg, const std::string& operationType);
     };
 }
