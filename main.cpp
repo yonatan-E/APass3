44686de417e969e9463950efe1b54ca40d874fff
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-30 18:46:36 +0300

    Updating main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
 #include "CacheManager.hpp"
 #include "MatrixOperationFactory.hpp"
 #include "HashOperationFactory.hpp"
+#include <iostream>
+#include <errno.h>
 
 using namespace operation;
 
 int main() {
+    try {
+        cache::CacheManager cache(3, "cache");
 
-    cache::CacheManager cache(3, "cache");
+        std::vector<std::string> command = {"matrix", "add", "matrix1.txt", "matrix2.txt", "stdout"};
+        std::unique_ptr<OperationFactory> factory;
+        if (command[0] == "matrix") {
+            factory = std::make_unique<MatrixOperationFactory>();
+        } else if (command[0] == "image") {
+            factory = std::make_unique<BitmapOperationFactory>();
+        } else if (command[0] == "hash") {
+            factory = std::make_unique<HashOperationFactory>();
+        }
 
-    std::vector<std::string> command = {"hash", "crc32", "hash1.txt", "stdout"};
-    HashOperationFactory factory = HashOperationFactory();
-    std::unique_ptr<Operation> operation = factory.createOperation(command, cache);
-    operation->writeOperationToFile(command[command.size() - 1]);
+        std::unique_ptr<Operation> operation = factory->createOperation(command, cache);
+        operation->writeOperationToFile(command[command.size() - 1]);
+    } catch (const std::exception& exc) {
+        std::cerr << exc.what() << '\n';
+    }
 }
