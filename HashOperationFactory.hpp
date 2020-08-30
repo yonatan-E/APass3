87f707834788eb407e2b77548cee55b448ecccff
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-30 18:54:43 +0300

    Adding HashOperationFactory.hpp

---
+++ HashOperationFactory.hpp
@@ -0,0 +1,23 @@
+#include "OperationFactory.hpp"
+#include "HashOperation.hpp"
+
+namespace operation {
+
+    class HashOperationFactory : public OperationFactory {
+
+        public:
+
+            virtual std::unique_ptr<Operation> createOperation(const std::vector<std::string>& command,
+                cache::CacheManager& cache) const override;
+
+        protected:
+
+            virtual bool isValidCommand(const std::vector<std::string>& command) const override;
+
+        private:
+
+            static uint32_t getOperationHashCode(const hash::CrcHash& arg);
+
+            static hash::CrcHash readHashFromFile(const std::string& pathToFile);
+    };
+}
