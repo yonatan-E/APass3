9f8f378f1079530e494f5fc89e39093875abe5a9
Ilan Prais <ilandovprais@gmail.com>
2020-08-30 16:16:51 +0300

    Test for Hash

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include "CacheManager.hpp"
 #include "MatrixOperationFactory.hpp"
+#include "HashOperationFactory.hpp"
 
 using namespace operation;
 
 int main() {
 
-    cache::CacheManager cache(3, "cache/info.txt");
+    cache::CacheManager cache(3, "cache");
 
-    std::vector<std::string> command = {"matrix", "add", "matrix1.txt", "matrix2.txt", "stdout"};
-    MatrixOperationFactory factory = MatrixOperationFactory();
+    std::vector<std::string> command = {"hash", "crc32", "hash1.txt", "stdout"};
+    HashOperationFactory factory = HashOperationFactory();
     std::unique_ptr<Operation> operation = factory.createOperation(command, cache);
-    operation->writeOperationToFile(command[4]);
+    operation->writeOperationToFile(command[command.size() - 1]);
 }
