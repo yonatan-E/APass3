c0eefef42d299cf1e774d273957002f7c2c478ca
Ilan Prais <ilandovprais@gmail.com>
2020-10-01 20:09:56 +0300

    removing unneccesary comments

--- src/factories/BitmapOperationFactory.cpp
+++ src/factories/BitmapOperationFactory.cpp
@@ -17,30 +17,25 @@ namespace operation {
 
         // creating a vector with the operation args, which are the operation type and the path to the input bitmap file
         std::vector<std::string> operationArgs(command.begin() + 1, command.begin() + 3);
-        // getting the hash code of the operation
+        
         uint32_t hashCode = calculateOperationHashCode(operationArgs);
 
-        // getting the result bitmap
+        
         std::unique_ptr<bitmap::Bitmap> result;
         // if the operation is already exist on the cache, we will take the result of the operation
         // from the cache, so we don't have to calculate it again.
         if (cache.contains(hashCode)) {
-            // getting the result bitmap from the cache
             result = std::make_unique<bitmap::Bitmap>(readBitmapFromFile(cache.getOperationFilePath(hashCode)));
-
             // if the operation isn't on the cache, we will calculate it and add it to the cache.
         } else {
-            // getting the input bitmap
             result = std::make_unique<bitmap::Bitmap>(readBitmapFromFile(command[2]));
-            // calculating the result bitmap
             command[1] == secondCommand ? result->turn() : result->gray();
         }
 
-        // getting the operation object
         BitmapOperation operation(hashCode, *result);
-        // adding the operation object to the cache
+        
         cache.load(operation);
-        // returning a smart pointer to the operation
+        
         return std::make_unique<BitmapOperation>(operation);        
     }
 
@@ -52,15 +47,10 @@ namespace operation {
             throw exceptions::InvalidCommandException();
         }
 
-        // getting the input bitmap
         auto input = readBitmapFromFile(operationArgs[1]);
         
-        // creating a string to create the hash code from it
-        std::string forHash = input.getData() + operationArgs[0];
-        
-        // creating a CrcHash hash object for creating the hashCode
-        hash::CrcHash hashTemp(forHash);
-        // calculating the hashCode using the crc32 algorithm
+        hash::CrcHash hashTemp(input.getData() + operationArgs[0]);
+       
         return hashTemp.applyAlgorithm();
     }
 
